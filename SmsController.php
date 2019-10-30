c80d0156da2c470b76d675eafd42adde0f1fad8e
Bashtem <topeadebass@yahoo.com>
2019-10-30 22:57:14 +0100

    Update

--- SmsController.php
+++ SmsController.php
@@ -4,7 +4,19 @@ namespace Wirepick\Smsapi;
 
 use Illuminate\Http\Request;
 
+
 class SmsController extends Controller
 {
-    //
+    public function send(Array $parameters){
+        $request = new GuzzleHttp\Client();
+        $res = $request->request('GET', 'https://sms.wirepick.com/httpsms/Send', [
+            'client' => $parameters['client'],
+             'password' => $parameters['password'],
+              'affiliate' =>$parameters['affiliate'],
+               'phone' => $parameters['phone'],
+                'text'=>$parameters['text'], 
+                'from' => $parameters['senderId']
+        ]);
+        return $res;
+    }
 }
